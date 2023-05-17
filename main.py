cda12cdc1509d28bb7f68649da7ebe95b24ad0b8
Mattias Björk <mattias.bjork.id1@gmail.com>
2023-05-17 13:57:28 +0200

    early stages

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-# Quizgame start
+class User:
+    def __init__(self, id, username):
+        self.id = id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+
+user_1 = User("001", "snake")
+user_2 = User("002", "worm")
+
+user_1.follow(user_2)
+
+print(user_1.followers)
+print(user_2.followers)
